b890b197d93539970cf41565c02a327905474cf5
Joe Mirizio <joe@joemirizio.com>
2012-04-19 08:58:11 -0400

    Fixed read/writeObject methods

--- src/Node.java
+++ src/Node.java
@@ -7,39 +7,44 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 public class Node implements Serializable {
-    
-    private static final String FILENAME = "data";
-    
-    protected HashMap<Integer, MusicObject> data;
-    
-    
-    public Node() { 
-        this.data = new HashMap<Integer, MusicObject>();
-    }
-    
-    public void addMusicObject(MusicObject song) {
-        this.data.put(song.getId(), song); 
-    }
-    
-    protected void writeToFile() {
-        try {
-            FileOutputStream fos = new FileOutputStream(FILENAME);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            
-            oos.write(this.data);
-            
-            oos.close();
-        } catch (IOException e) { e.printStackTrace(); }
-    }
-    
-    protected void readFromFile() {
-        try {
-            FileInputStream fis = new FileInputStream(FILENAME);
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            
-            this.data = (HashMap<Integer, MusicObject>)ois.read(data);
-            
-            ois.close();
-        } catch (IOException e) { e.printStackTrace(); }
-    }
+	
+	private static final String FILENAME = "data";
+	
+	protected HashMap<Integer, MusicObject> data;
+	
+	
+	public Node() { 
+		this.data = new HashMap<Integer, MusicObject>();
+	}
+	
+	public void addMusicObject(MusicObject song) {
+		this.data.put(song.getId(), song); 
+	}
+	
+	protected void writeToFile() {
+		try {
+			FileOutputStream fos = new FileOutputStream(FILENAME);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			
+			oos.writeObject(this.data);
+			
+			oos.close();
+		} catch (IOException e) { e.printStackTrace(); }
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected void readFromFile() {
+		try {
+			FileInputStream fis = new FileInputStream(FILENAME);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			
+			this.data = (HashMap<Integer, MusicObject>)ois.readObject();
+			
+			ois.close();
+		} catch (IOException e) { 
+			e.printStackTrace(); 
+		} catch (ClassNotFoundException e) { 
+			e.printStackTrace();
+		}
+	}
 }
