2bbff9d0dfd97534a6ddb7d543e9870bd8719da1
Joe Mirizio <joe@joemirizio.com>
2012-05-06 03:55:56 -0400

    Added pseudo code for future methods. Added Node and NodeServer documentation.

--- src/NodeServer.java
+++ src/NodeServer.java
@@ -1,3 +1,8 @@
+/**
+ * CSC 445
+ * Project 3
+ */
+
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -10,10 +15,25 @@ public class NodeServer extends Thread {
 	public static final String DEFAULT_SERVER_LIST = "servers.txt";
 	public static final int SERVER_CONNECT_TIMEOUT = 1000;
 
+	/**
+	 * The socket connection for listening.
+	 */
 	private ServerSocket socket;
+	/**
+	 * A list of all connected nodes.
+	 */
 	private List<Socket> servers;
+	/**
+	 * The persistent data node.
+	 */
 	private Node node;
 
+
+	/**
+	 * Constructor specifying the port and the initial server list.
+	 * @param port The port.
+	 * @param server_list_file The initial list of all known nodes.
+	 */
 	public NodeServer(int port, String server_list_file) {
 		/* Create TCP socket */
 		try {
@@ -39,13 +59,53 @@ public class NodeServer extends Thread {
 			} catch (IOException e) { System.out.format("[!] %s: %s:%d\n", e.getMessage(), svr_host, svr_port); }
 		}
 
+		/* Initialize node */
+		this.node = new Node();
+		this.node.readFromFile();
+
 		this.start();
 	}
 
+	/**
+	 * Processes the requests.
+	 */
 	public void run() {
-
+		while (true) {
+			// @TODO Receive requests and process actions
+			// @TODO Check status of all other nodes
+		}
 	}
 
+	/**
+	 * Adds a MusicObject to the node and updates all connected nodes.
+	 * @param song The song to be added.
+	 */
+	private void addMusicObject(MusicObject song) {
+		this.node.addMusicObject(song);
+		// @TODO Send updates to all nodes (possibly do a consensus check)
+	}
+
+	/**
+	 * Queries all nodes and returns the appropriate dataset.
+	 * @param query The query.
+	 * @return The list of all appropriate MusicOjects.
+	 */
+	private List<MusicObject> queryAll(String query) {
+		List<MusicObject> results = new ArrayList<MusicObject>();//this.node.query(query);
+		for (Socket sock : this.servers) {
+			// @TODO Send GET to all other nodes, receive List<MusicObject> through OOS
+		}
+		// @TODO Perform consensus
+	
+		// @TODO Limit and return results
+		return results;
+	}
+
+	/**
+	 * Starts the server.
+	 * @param args[0] The port to listen on.
+	 * @param args[1] The initial list of all other known nodes.
+	 */
 	public static void main(String[] args) {
 		int port = (args.length > 0) ? Integer.parseInt(args[0]) : DEFAULT_PORT;
 		String server_list_file = (args.length > 1) ? args[1] : DEFAULT_SERVER_LIST;
