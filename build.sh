976ae4f70c7194aefece0da4c2623a0450074630
Jacob Peck <suschord@suspended-chord.info>
2012-05-08 10:50:50 -0400

    Updated build scripts to support altair/gee/moxie, and updated server file to all use the same port.

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ if [ ! -d $CLASS_DIR ]; then
   mkdir $CLASS_DIR
 fi
 
-if [[ $OSTYPE == linux-gnu ]] || [[ ${OSTYPE//[0-9.]/} == darwin ]]; then
+if [[ $OSTYPE == solaris ]]; then
+  /opt/jdk1.7.0/bin/javac -cp $CP -sourcepath $SRC_DIR -d $CLASS_DIR $1 $SRC_DIR/*.java && echo "Done."
+elif [[ $OSTYPE == linux-gnu ]] || [[ ${OSTYPE//[0-9.]/} == darwin ]]; then
   javac -cp $CP -sourcepath $SRC_DIR -d $CLASS_DIR $1 $SRC_DIR/*.java && echo "Done."
 elif [[ $OSTYPE == cygwin ]]; then
   javac -cp `cygpath -wp $CP` -sourcepath $SRC_DIR -d $CLASS_DIR $1 $SRC_DIR/*.java && echo "Done."
--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 CP=classes/.:.
 
 echo "Running..."
-if [[ $OSTYPE == linux-gnu ]] || [[ ${OSTYPE//[0-9.]/} == darwin ]]; then
+if [[ $OSTYPE == solaris ]]; then
+  /opt/jdk1.7.0/bin/java -cp $CP $1
+elif [[ $OSTYPE == linux-gnu ]] || [[ ${OSTYPE//[0-9.]/} == darwin ]]; then
   java -cp $CP $1
 elif [[ $OSTYPE == cygwin ]]; then
   java -cp `cygpath -wp $CP` $1
